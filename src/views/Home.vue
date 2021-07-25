243709f4e1b4080578da48fddceed5bd0f35afe4
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-07-25 10:28:07 -0500

    add puppies index action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,34 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="puppy in puppies">
+      <p> {{ puppy.id }} </p>
+      <p> {{ puppy.name }} </p>
+      <p>--------------------------------------------------</p>
+    </div>
   </div>
 </template>
 <style></style>
 <script>
+import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Look at all the Puppies",
+      puppies: []
     };
   },
-  created: function() {},
-  methods: {}
+  created: function() {
+    this.indexPuppies()
+  },
+  methods: {
+    indexPuppies: function () {
+      console.log("puppies index...")
+      axios.get('http://localhost:3000/puppies').then((response) => {
+      console.log(response);
+      this.puppies = response.data;
+      }) ;
+    },
+  }
 };
 </script>
