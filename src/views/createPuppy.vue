90b0bfe1c3f39e9e484b05f97925b07dbe9f42d4
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-07-25 11:27:25 -0500

    add creat puppy

--- src/views/createPuppy.vue
+++ src/views/createPuppy.vue
@@ -1,20 +1,34 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <p> <input type="text" v-model="newPuppyParams.name"
+    <p> Name:<input type="text" v-model="newPuppyParams.name"> </p>
+    <p> Age:<input type="text" v-model="newPuppyParams.age"> </p>
+    <p> Breed:<input type="text" v-model="newPuppyParams.breed"> </p>
+    <button v-on:click="createPuppy()"> Add Puppy </button>
   </div>
 </template>
 
 <style></style>
 
 <script>
+  import axios from "axios"
   export default {
     data: function () {
       return {
         message: "Add New Puppy",
+        newPuppyParams: {}
       };
     },
     created: function () {},
-    methods: {},
+    methods: {
+      createPuppy: function () {
+        console.log("creating puppy....")
+        axios.post('http://localhost:3000/puppies', this.newPuppyParams).then((response) => {
+          console.log(response.data)
+          this.newPuppyParams = {};
+          this.$router.push("/");
+        });
+      }
+    },
   };
 </script>
