b5eae3908109dc6d1db64223d7b5a11cf7cdd53f
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-07-25 11:07:17 -0500

    add new puppy page

---
+++ src/views/createPuppy.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <p> <input type="text" v-model="newPuppyParams.name"
+  </div>
+</template>
+
+<style></style>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        message: "Add New Puppy",
+      };
+    },
+    created: function () {},
+    methods: {},
+  };
+</script>
